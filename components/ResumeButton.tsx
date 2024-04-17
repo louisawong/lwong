94f756a06d82bdb0d5164d95ba809af086465915
Louisa Wong <louisawong95@hotmail.com>
2024-04-17 00:46:21 -0400

    Resume button on hover colour change

--- components/ResumeButton.tsx
+++ components/ResumeButton.tsx
@@ -9,7 +9,7 @@ export function ResumeButton() {
   return (
     <a href="./files/Louisa-Wong-2024.pdf" download className="hamburger_choices_option">
         <div className={styles[`button-${themeContext.background === '#283618' ? 'dark' : 'light'}`]}>
-        <p className={styles.buttonCTA}>Resume</p>
+          <p className={styles.buttonCTA}>Resume</p>
         </div>
     </a>
   )
